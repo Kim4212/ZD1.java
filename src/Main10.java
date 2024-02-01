3028a2dda08af2e735af1cd0af5a7c2bb7bd1a54
kim <Kmi.podolyak@gmail.com>
2024-02-01 20:37:52 +0300

    Stydy

---
+++ src/Main10.java
@@ -0,0 +1,20 @@
+public class Main10 {
+    public static void main(String[] args) {
+        double[][] numbersForMatrixA = {
+                {7, 5, 6, 8},
+                {1, 2, 3, 4},
+                {0, 3, 9, 4},
+        };
+
+        Matrix a = new Matrix(numbersForMatrixA);
+        System.out.println(a.getRows());
+//        System.out.println(a.getColumns());
+//        System.out.println(a,getValueAt());
+
+//        System.out.println(a.getValueAt());
+//        a.setValueAt(1, 1, 2);
+//        System.out.println(a);
+
+
+    }
+}
